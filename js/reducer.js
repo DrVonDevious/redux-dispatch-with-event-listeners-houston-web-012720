6442994e08fc2281c139aac52e7fe3ebc6f384aa
DrVonDevious <drvondevious@gmail.com>
2020-04-17 10:26:00 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,34 @@
-// add code snippets from README
+let state
+
+let plusButton = document.getElementById('plus-button')
+let minusButton = document.getElementById('minus-button')
+
+const reducer = (state = {count: 0}, action) => {
+  switch(action.type) {
+    case "INCREASE_COUNT":
+      return {count: state.count + 1}
+    case "DECREASE_COUNT":
+      return {count: state.count - 1}
+    default: return state
+  }
+}
+
+const dispatch = (action) => {
+  state = reducer(state, action)
+  render()
+}
+
+plusButton.addEventListener('click', () => {
+  dispatch({type: "INCREASE_COUNT"})
+})
+
+minusButton.addEventListener('click', () => {
+  dispatch({type: "DECREASE_COUNT"})
+})
+
+const render = () => {
+  let container = document.getElementById('container')
+  container.textContent = state.count
+}
+
+dispatch({type: "@@INIT"})
